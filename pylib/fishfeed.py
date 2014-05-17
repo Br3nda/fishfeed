38371f3c9be8a6e1bbae139891b431f17ade673c
Brenda Wallace <shiny@cpan.org>
2014-05-17 17:21:27 +1200

    client to send recorded values to the server

--- pylib/fishfeed.py
+++ pylib/fishfeed.py
@@ -2,7 +2,14 @@ import logging
 
 logger = logging.getLogger('fishfeed')
 
+COLLECTOR_URL='http://127.0.0.1:8081'
+
+
+
 class FishTank(object):
+
+    def __init__(self):
+        self.recent_values = []
     @property
     def fishes(self):
         pass
@@ -11,11 +18,33 @@ class FishTank(object):
         """Starts monitoring the fish tank"""
         
         pass
+    def record_value(self, value):
+        """Adds a sample to the list of things to send to the server"""
+        self.recent_values.append(time.time(), value)
+        
+        #is it time to send to the server?
+        if (self.recent_values) >= 100:
+            self.send_values()
+    
 
+    def send_values(self):
+        """Send a batch of values to the server"""
+        pid = os.fork()
+        if pid:
+            self.recent_values = []
+            return
+        try:
+            url = '{collector_url}/api/value'.format(collector_url=COLLECTOR_URL)
+            data = {'tank_id': self.tank_id, 'values': {}}
+            for sample_time, value in self.recent_values:
+                data['values'][str(sample_time)] = value
+                
+            response = requests.post(url, data)
+            logger.info(response.text)
 
-class Fish(object):
-    @property
-    def tank(self):
-       pass
+        except Exception, e:
+            logger.exception(e)
+        finally:
+            os._exit(0)
+        
 
- 
