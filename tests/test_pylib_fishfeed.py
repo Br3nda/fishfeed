8feee2fbb19d105c2e848156e6f20818462426a7
Brenda Wallace <shiny@cpan.org>
2014-05-17 16:40:52 +1200

    TESTS! are like pokemon. gotta get them all

---
+++ tests/test_pylib_fishfeed.py
@@ -0,0 +1,21 @@
+import unittest
+
+class TestFishTank(unittest.TestCase):
+    def test_fishes(self):
+        # fish_tank = FishTank()
+        # self.assertEqual(expected, fish_tank.fishes())
+        assert False # TODO: implement your test here
+
+    def test_monitor(self):
+        # fish_tank = FishTank()
+        # self.assertEqual(expected, fish_tank.monitor())
+        assert False # TODO: implement your test here
+
+class TestFish(unittest.TestCase):
+    def test_tank(self):
+        # fish = Fish()
+        # self.assertEqual(expected, fish.tank())
+        assert False # TODO: implement your test here
+
+if __name__ == '__main__':
+    unittest.main()
